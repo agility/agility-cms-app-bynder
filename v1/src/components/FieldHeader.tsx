e1b4dc0d1607f8d02d3f3f19f4ac5533a2c2235f
Dakotah Godfrey <dakotah.godfrey.dev@gmail.com>
2024-02-23 14:43:34 -0500

    Update dependencies and fix formatting in layout and metadata components

--- v1/src/components/FieldHeader.tsx
+++ v1/src/components/FieldHeader.tsx
@@ -1,60 +1,71 @@
-import React from "react"
-import {Button, ButtonDropDown} from "@agility/plenum-ui"
-import { IconChevronDown } from "@tabler/icons-react"
+import React from "react";
+import { Button, ButtonDropdown } from "@agility/plenum-ui";
+import { IconChevronDown } from "@tabler/icons-react";
 
 interface Props {
-	fieldConfig: {
-		readOnly: boolean
-	}
-	attachment: any
-	handleRemove: () => void
-	handleSelect: () => void
+  fieldConfig: {
+    readOnly: boolean;
+  };
+  attachment: any;
+  handleRemove: () => void;
+  handleSelect: () => void;
 }
 
-const FieldHeader = ({fieldConfig, attachment, handleRemove, handleSelect}: Props) => {
-	return (
-		<div className="flex w-full items-center justify-between pb-1 font-muli">
-			<div className="flex items-center" />
-			{fieldConfig.readOnly !== true && (
-				<div className="top-buttons">
-					{attachment ? (
-						<ButtonDropDown
-							button={{
-								icon: "CollectionIcon",
-								label: "Browse",
-								size: "sm",
-								onClick: () => handleSelect(),
-								type: "secondary",
-							}}
-							dropDown={{
-								IconElement: () => (
-									<IconChevronDown className="h-5 w-5 text-purple-600" stroke={2} />
-								),
-								items: [
-									[
-										{
-											icon: "TrashIcon",
-											label: "Remove",
-											isEmphasized: true,
-											onClick: () => handleRemove(),
-										},
-									],
-								],
-							}}
-						/>
-					) : (
-						<Button
-							icon="FolderDownloadIcon"
-							label="Browse"
-							size="sm"
-							onClick={() => handleSelect()}
-							type="secondary"
-						/>
-					)}
-				</div>
-			)}
-		</div>
-	)
-}
+const FieldHeader = ({
+  fieldConfig,
+  attachment,
+  handleRemove,
+  handleSelect,
+}: Props) => {
+  return (
+    <div className="flex w-full items-center justify-between pb-1 font-muli">
+      <div className="flex items-center" />
+      {fieldConfig.readOnly !== true && (
+        <div className="top-buttons">
+          {attachment ? (
+            <ButtonDropdown
+              button={{
+                icon: "CollectionIcon",
+                label: "Browse",
+                size: "sm",
+                onClick: () => handleSelect(),
+                actionType: "secondary",
+              }}
+              dropDown={{
+                label: "",
+                id: "dropdown-1",
+                CustomDropdownTrigger: (
+                  <IconChevronDown
+                    className="h-5 w-5 text-purple-600"
+                    stroke={2}
+                  />
+                ),
+                items: [
+                  [
+                    {
+                      icon: "TrashIcon",
+                      label: "Remove",
+                      isEmphasized: true,
+                      onClick: () => handleRemove(),
+                      key: "trash",
+                    },
+                  ],
+                ],
+              }}
+            />
+          ) : (
+            <Button
+              icon="FolderDownloadIcon"
+              label="Browse"
+              size="sm"
+              onClick={() => handleSelect()}
+              actionType="secondary"
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default FieldHeader
+export default FieldHeader;
--- v1/src/components/Metadata.tsx
+++ v1/src/components/Metadata.tsx
@@ -1,62 +1,65 @@
-
 import fileSizeFromBytes from "../utils/fileSizeFromBytes";
 import { TextInput } from "@agility/plenum-ui";
 import { MetaRow } from "./MetaRow";
 
 interface Props {
-	attachment: any
-	isReadonly: boolean
-	handleAltTextChange: any
-	isImage: any
+  attachment: any;
+  isReadonly: boolean;
+  handleAltTextChange: any;
+  isImage: any;
 }
 
-const Metadata = ({ attachment, isReadonly, handleAltTextChange, isImage }: Props) => {
+const Metadata = ({
+  attachment,
+  isReadonly,
+  handleAltTextChange,
+  isImage,
+}: Props) => {
+  const orig =
+    attachment && attachment.files ? attachment.files.original : null;
 
-	const orig = attachment && attachment.files ? attachment.files.original : null
+  let fileName = attachment.originalUrl || "";
+  if (fileName.includes("/")) {
+    fileName = fileName.substring(fileName.lastIndexOf("/") + 1);
+  }
 
-	let fileName = attachment.originalUrl || ""
-	if (fileName.includes("/")) {
-		fileName = fileName.substring(fileName.lastIndexOf("/") + 1)
-	}
+  console.log("orig", orig);
 
-	console.log("orig", orig)
+  return (
+    <div className="mx-6 w-full flex-1 font-muli bg-white min-w-[275px]">
+      {isImage && (
+        <TextInput
+          type="text"
+          label="Alt Text"
+          placeholder="Enter alt text"
+          className="form-control agility-attachment-alt"
+          value={attachment?.alt || attachment?.name || ""}
+          handleChange={(val: string) => handleAltTextChange(val)}
+          isReadonly={isReadonly}
+        />
+      )}
 
-
-	return (
-		<div className="mx-6 w-full flex-1 font-muli bg-white min-w-[275px]">
-			{isImage && (
-				<TextInput
-					type="text"
-					label="Alt Text"
-					placeholder="Enter alt text"
-					className="form-control agility-attachment-alt"
-					value={attachment?.alt || attachment?.name || ""}
-					onChange={(val) => handleAltTextChange(val)}
-					isReadonly={isReadonly}
-				/>
-			)}
-
-			<MetaRow label={"Size"} value={fileSizeFromBytes(orig?.fileSize)} />
-			<>
-				{orig?.width > 0 && <MetaRow label={"Width"} value={orig?.width} />}
-				{orig?.height > 0 && <MetaRow label={"Height"} value={orig?.height} />}
-			</>
-			<MetaRow
-				label={"URL"}
-				className={"border-b-0 border-b-transparent"}
-				value={
-					<a
-						className="block break-all text-purple-600 line-clamp-1 hover:underline"
-						href={attachment.originalUrl}
-						target="_blank"
-						rel="noreferrer"
-					>
-						{fileName || "Asset URL"}
-					</a>
-				}
-			/>
-		</div>
-	)
-}
+      <MetaRow label={"Size"} value={fileSizeFromBytes(orig?.fileSize)} />
+      <>
+        {orig?.width > 0 && <MetaRow label={"Width"} value={orig?.width} />}
+        {orig?.height > 0 && <MetaRow label={"Height"} value={orig?.height} />}
+      </>
+      <MetaRow
+        label={"URL"}
+        className={"border-b-0 border-b-transparent"}
+        value={
+          <a
+            className="block break-all text-purple-600 line-clamp-1 hover:underline"
+            href={attachment.originalUrl}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {fileName || "Asset URL"}
+          </a>
+        }
+      />
+    </div>
+  );
+};
 
 export default Metadata;
