f2e2c3ee4aca0ff65d7a7dd366bdb727f4fa166a
DerekAgility <derek@agilitycms.com>
2025-07-17 15:55:32 -0400

    Aded transformBaseUrl to derivates to bynder

--- v1/src/components/AssetSelector.tsx
+++ v1/src/components/AssetSelector.tsx
@@ -1,7 +1,7 @@
-"use client"
-import {useAgilityAppSDK, closeModal} from "@agility/app-sdk"
-import {CompactView, Login, assetType} from "@bynder/compact-view"
-import {useMemo} from "react"
+"use client";
+import { useAgilityAppSDK, closeModal } from "@agility/app-sdk";
+import { CompactView, Login } from "@bynder/compact-view";
+import { useMemo } from "react";
 
 const assetFieldSelection = `
   name
@@ -10,53 +10,54 @@ const assetFieldSelection = `
   derivatives {
     thumbnail
     webImage
+	transformBaseUrl
   }
   ... on Video {
     previewUrls
   }
-`
+`;
 
 export default function BynderAssetSelector() {
-	const {initializing, appInstallContext, modalProps} = useAgilityAppSDK()
+  const { initializing, appInstallContext, modalProps } = useAgilityAppSDK();
 
-	const config = appInstallContext?.configuration
+  const config = appInstallContext?.configuration;
 
-	const bynderUrl = useMemo(() => {
-		return config?.bynderurl
-	}, [config])
+  const bynderUrl = useMemo(() => {
+    return config?.bynderurl;
+  }, [config]);
 
-	if (initializing) {
-		return null
-	}
+  if (initializing) {
+    return null;
+  }
 
-	if (!bynderUrl) {
-		return <div>Please ensure this app has been properly configured.</div>
-	}
+  if (!bynderUrl) {
+    return <div>Please ensure this app has been properly configured.</div>;
+  }
 
-	const assetType = modalProps?.assetType || "IMAGE"
+  const assetType = modalProps?.assetType || "IMAGE";
 
-	return (
-		<div id="media-container" className="flex flex-col h-full min-h-0">
-			<div className="relative flex-1 overflow-hidden">
-				<Login
-					portal={{
-						url: bynderUrl,
-						editable: false,
-					}}
-				>
-					<CompactView
-						mode="SingleSelect"
-						language="en_US"
-						assetTypes={assetType && [assetType]}
-						isContainerMode
-						onSuccess={(assets) => {
-							const [asset] = assets
-							closeModal(asset)
-						}}
-						assetFieldSelection={assetFieldSelection}
-					/>
-				</Login>
-			</div>
-		</div>
-	)
+  return (
+    <div id="media-container" className="flex flex-col h-full min-h-0">
+      <div className="relative flex-1 overflow-hidden">
+        <Login
+          portal={{
+            url: bynderUrl,
+            editable: false,
+          }}
+        >
+          <CompactView
+            mode="SingleSelect"
+            language="en_US"
+            assetTypes={assetType && [assetType]}
+            isContainerMode
+            onSuccess={(assets) => {
+              const [asset] = assets;
+              closeModal(asset);
+            }}
+            assetFieldSelection={assetFieldSelection}
+          />
+        </Login>
+      </div>
+    </div>
+  );
 }
